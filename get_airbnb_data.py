01897940d95e692055b1f2660c9b2da7ea4f07c8
geosag <geo.sagris@gmail.com>
2026-06-26 10:30:20 +0300

    Change logic in script, GitHub Actions scheduler and requirements

--- get_airbnb_data.py
+++ get_airbnb_data.py
@@ -4,7 +4,6 @@
 from datetime import datetime, date
 from sqlalchemy import create_engine, text
 import time
-import random
 import logging
 import os
 import sys
@@ -16,6 +15,30 @@ import pandas as pd
 ########## GLOBAL VARIABLES #############
 #----------------------------------------
 url = 'https://insideairbnb.com/get-the-data/'
+csv_types = ['listings', 'reviews'] # types of csv files to gather
+city = 'athens' # (in lowercase) to get the data for the city interested in
+listings_cols = [
+    "id",
+    "host_id",
+    "host_url",
+    "host_name",
+    "neighbourhood_cleansed",
+    "latitude",
+    "longitude",
+    "room_type",
+    "price",
+    "minimum_nights",
+    "availability_365",
+    "number_of_reviews",
+    "last_review",
+    "review_scores_rating",
+    "license",
+    "calculated_host_listings_count",
+]
+reviews_cols = [
+    "listing_id",
+    "date",
+]
 #----------------------------------------
 ############## FUNCTIONS ################
 #----------------------------------------
@@ -38,43 +61,52 @@ def query_db(query, connection):
 #----------------------------------------
 # main logic
 #----------------------------------------
-def main(connection):
+def main(city, connection):
+    link_found = False # boolean for link found
+    new_data_available = False # boolean for available new data
     response = requests.get(url)
     if response.status_code == 200:
         html = BeautifulSoup(response.content, 'html.parser') # get the html
-        city_section = html.find('h2', attrs={"class": None}) # find the first h2 heading with no class, targeting the 'Data Downloads' section
-        cities = city_section.find_all_next('h3') # get a list of all the cities in the section
-        for city in cities:
-            if 'Athens' in str(city): # targeting Athens city
-                date_string = str(str(city.find_next('h4')).strip('<h4>')).split('<span>')[0] # get the date substring of the tag, strip the tags and everything else besides the date included
-                # -----------------------------------------------------------------------------------------------
-                # --- decide what the script should do
-                # --- if the latest available date for data from web matches the last inserted date in the database and
-                # --- no mismatches between rows occur in the tables since last insert then the program ends as no newest data is actually available
-                # -----------------------------------------------------------------------------------------------
-                latest_date_web = datetime.strptime(date_string, '%d %B, %Y').date() # convert latest date from web to valid date object
-                result = query_db(text("SELECT MAX(last_update) FROM airbnb_data_latest_info;"), connection) # get the latest date from the database
-                latest_date_db = result.fetchone()[0]
-                # -----------------------------------------------------------------------------------------------
-                result = query_db(text("SELECT row_number_listings FROM airbnb_data_latest_info WHERE last_update = (SELECT MAX(last_update) FROM airbnb_data_latest_info);"), connection) # get the number of rows that were last inserted for listings table
-                last_row_number_listings = result.scalar()
-                result = query_db(text("SELECT row_number_reviews FROM airbnb_data_latest_info WHERE last_update = (SELECT MAX(last_update) FROM airbnb_data_latest_info);"), connection) # get the number of rows that were last inserted for reviews table
-                last_row_number_reviews = result.scalar()
-                # -----------------------------------------------------------------------------------------------
-                result = query_db(text("SELECT COUNT(*) FROM listings;"), connection) # get the current number of rows for listings table
+        for csv_type in csv_types:
+            # count previous rows (stored in the database) -------------------------------------------------------------------------
+            result = query_db(text(f"SELECT row_number_{csv_type} FROM airbnb_data_latest_info WHERE last_update = (SELECT MAX(last_update) FROM airbnb_data_latest_info);"), connection) # get the number of rows that were last inserted for each table
+            if csv_type == 'listings':
+                previous_row_number_listings = result.scalar()
+            else:
+                previous_row_number_reviews = result.scalar()
+            # count current rows ---------------------------------------------------------------------------------------------------
+            result = query_db(text(f"SELECT COUNT(*) FROM {csv_type};"), connection) # get the current number of rows for each table
+            if csv_type == 'listings':
                 current_row_number_listings = result.scalar()
-                result = query_db(text("SELECT COUNT(*) FROM reviews;"), connection) # get the current number of rows for reviews table
+            else:
                 current_row_number_reviews = result.scalar()
-                # -----------------------------------------------------------------------------------------------
-                if latest_date_web == latest_date_db and last_row_number_listings == current_row_number_listings and last_row_number_reviews == current_row_number_reviews:
-                    logging.info('No newest data available!')
-                else: # get the data
-                    csv_type = 'listings' # start with listings
-                    while True:
-                        time.sleep(random.uniform(30, 60)) # sleeps between requests for extra safety
-                        # csv link address
-                        csv_url = f"https://data.insideairbnb.com/{str(str(str(city).strip('</h3>')).split(', ')[2]).lower()}/{str(str(str(city).strip('</h3>')).split(', ')[1]).lower()}/{str(str(str(city).strip('</h3>')).split(', ')[0]).lower()}/{latest_date_web}/visualisations/{csv_type}.csv"
-                        df = pd.read_csv(csv_url) # using pandas library to manipulate the data
+        # ----------------------------------------------------------------------------------------------------------------------------------------------
+        for csv_type in csv_types:
+            for a in html.find_all("a", href=True):
+                if city in a["href"] and f"{csv_type}.csv.gz" in a["href"]: # to target the specific compressed files
+                    link_found = True # there is new data to gather
+                    logging.info(f"The link for {csv_type} was found!")
+                    csv_url = a["href"]
+                    date_string = csv_url.split("/")[6] # extract the date from the link
+                    # ----------------------------------------------------------------------------------------------------------------------------------
+                    latest_date_web = date.fromisoformat(date_string) # convert latest date from web to valid date object
+                    result = query_db(text("SELECT MAX(last_update) FROM airbnb_data_latest_info;"), connection) # get the latest date from the database
+                    latest_date_db = result.fetchone()[0]
+                    # ----------------------------------------------------------------------------------------------------------------------------------
+                    # --- if the latest available date for data from web matches the last inserted date in the database and
+                    # --- no mismatches between rows occur in the tables since last insert then the program ends as no newest data is actually available
+                    # ----------------------------------------------------------------------------------------------------------------------------------
+                    if latest_date_web == latest_date_db and previous_row_number_listings == current_row_number_listings and previous_row_number_reviews == current_row_number_reviews:
+                        logging.info('No newest data available!')
+                        break
+                    else: # get the data
+                        new_data_available = True # there is new data available to gather
+                        if csv_type == 'listings':
+                            columns = listings_cols
+                        else:
+                            columns = reviews_cols
+                        query_db(text(f"DELETE FROM {csv_type};"), connection) # delete everything from the table to start fresh
+                        df = pd.read_csv(csv_url, compression="gzip", usecols=columns) # using pandas library to manipulate the data and which columns to be used
                         if csv_type == 'listings':
                             column = 'id'
                         else:
@@ -100,10 +132,9 @@ def main(connection):
                         df.to_sql(f"{csv_type}", con=connection, if_exists="append", index=False, method="multi", chunksize=10000) # perform the insert
                         logging.info(f"{len(df)} rows successfully inserted into {csv_type} table!")
                         if csv_type == 'listings':
-                            listings_rows = len(df) # assign the number of rows inserted for listings to listings_rows variable 
-                            csv_type = 'reviews' # repeat for reviews
-                        else: # id all done
-                            reviews_rows = len(df) # assign the number of rows inserted for listings to reviews_rows variable
+                            listings_rows = len(df) # assign the number of rows inserted for listings to listings_rows variable
+                        else:
+                            reviews_rows = len(df) # assign the number of rows inserted for reviews to reviews_rows variable
                             # --- using a helper list to insert the below info to airbnb_data_latest_info table
                             helper_list = []
                             info = {'last_update': latest_date_web, 'row_number_listings': listings_rows, 'row_number_reviews': reviews_rows}
@@ -111,10 +142,11 @@ def main(connection):
                             df = pd.DataFrame(helper_list)
                             df.to_sql("airbnb_data_latest_info", con=connection, if_exists="append", index=False)
                             logging.info(f"The database was updated with the latest date of available data ({latest_date_web})")
-                            break # break from while loop and then from for loop and end normally
-                break
+            if link_found == False and new_data_available == False:
+                logging.info(f"No relevant {csv_type} link was found for the requested city! Process will end")
+                break # abort in any iteration
     else:
-         raise Exception(f"Request (url) failed with HTTP response status code: {response.status_code}")
+         raise Exception(f"Request failed with HTTP response status code: {response.status_code}")
 #----------------------------------------
 #########################################
 #----------------------------------------
@@ -125,7 +157,8 @@ if __name__ == "__main__":
         format = "%(asctime)s - %(levelname)s - %(message)s",
         handlers = [logging.StreamHandler(sys.stdout)] #logs streamed directly to GitHub Actions console
     )
-    connection_string = os.getenv("CONNECTION_STRING")
+    # connection_string = os.getenv("CONNECTION_STRING")
+    connection_string = "REMOVED"
     if connection_string:
         engine = create_engine(connection_string)
         db_test = check_connection()
@@ -133,7 +166,7 @@ if __name__ == "__main__":
             logging.info('Connection with database was established!')
             try:
                 with engine.begin() as connection:
-                    main(connection)
+                    main(city, connection)
             except Exception:
                 logging.exception('Program ended abnormally!')
                 sys.exit(1)
